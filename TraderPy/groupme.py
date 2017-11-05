ca0622caa4f9e531bd92a99c47302b0d3edf245f
Zack White <zackwhiteal@gmail.com>
2017-11-05 13:29:20 -0500

    Send notifications to GroupMe

--- TraderPy/groupme.py
+++ TraderPy/groupme.py
@@ -1,1 +1,18 @@
 import requests
+import yaml
+import os
+
+
+class groupme:
+
+    def __init__(self):
+        dir = os.path.dirname(__file__)
+        filename = os.path.join(dir, 'secrets.yml')
+
+        with open(filename, 'r') as ymlfile:
+            self.secrets = yaml.load(ymlfile)
+
+    def send_message(self, message):
+        r = requests.post(self.secrets['groupme']['url'], data={
+                          'bot_id': self.secrets['groupme']['bot_id'],
+                          'text': message})
---
+++ TraderPy/test_groupme.py
@@ -0,0 +1,4 @@
+from groupme import groupme
+
+bot = groupme()
+bot.send_message('TraderPy test of GroupMe notifications')
